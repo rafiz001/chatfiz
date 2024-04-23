0783cbefc530ec917c0d368ad21691053c844dda
rafiz001 <rafiz001@protonmail.ch>
2024-04-23 08:09:49 +0600

    added some extra contact list

--- src/components/MidPanel.jsx
+++ src/components/MidPanel.jsx
@@ -9,6 +9,14 @@ export default function(){
     <ContactList  user={123} selected={selectedContact} setSelectedContact={setSelectedContact}/>
     <ContactList   user={456} selected={selectedContact} setSelectedContact={setSelectedContact}/>
     <ContactList   user={789} selected={selectedContact} setSelectedContact={setSelectedContact}/>
+    <ContactList   user={34} selected={selectedContact} setSelectedContact={setSelectedContact}/>
+    <ContactList   user={7832432439} selected={selectedContact} setSelectedContact={setSelectedContact}/>
+    <ContactList   user={7834539} selected={selectedContact} setSelectedContact={setSelectedContact}/>
+    <ContactList   user={783534545459} selected={selectedContact} setSelectedContact={setSelectedContact}/>
+    <ContactList   user={7865656759} selected={selectedContact} setSelectedContact={setSelectedContact}/>
+    <ContactList   user={78687879} selected={selectedContact} setSelectedContact={setSelectedContact}/>
+    <ContactList   user={78798799} selected={selectedContact} setSelectedContact={setSelectedContact}/>
+    <ContactList   user={2323} selected={selectedContact} setSelectedContact={setSelectedContact}/>
 
        
     

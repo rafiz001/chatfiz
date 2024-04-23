6f6626d2c602994564118fef07f7040534e51312
rafiz001 <rafiz001@protonmail.ch>
2024-04-23 07:05:05 +0600

    fixed scrollbars styles to hidden.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -9,7 +9,7 @@ function App() {
       <div className="flex-[1]  bg-slate-800 h-[100vh]  flex flex-col justify-between" >
        <LeftPanel/> 
       </div>
-      <div className="flex-[6] bg-slate-700 h-[100vh] overflow-y-auto">
+      <div className="flex-[6] bg-slate-700 h-[100vh] overflow-y-auto ">
         <MidPanel/>
       </div>
       <div className="flex-[11] bg-gray-600 ">chat history</div>
